64624088d47f6f505d4e5386211cb093b46a9848
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2024-12-25 21:22:15 +0100

    Added a toggle on the first Element and last element

--- js/index.js
+++ js/index.js
@@ -2,3 +2,12 @@
 
 // the first paragraph to red
 // the second paragraph to yellow.
+
+let mainContent = document.getElementById("main-content");
+let btnChange = document.getElementById("btn-change-color");
+btnChange.addEventListener("click", () => {
+  let firstParagraph = mainContent.firstElementChild;
+  let lastParagraph = mainContent.lastElementChild;
+  firstParagraph.classList.toggle("red");
+  lastParagraph.classList.toggle("yellow");
+});
